f6019326fa30e81571c1e79c24cd53e42aefd9d9
root <root@rockylinux.PotantcevDP.ru>
2024-11-28 12:45:11 +0000

    Tests adding

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -16,4 +16,39 @@ public class Tests
 
         Assert.AreEqual(5, result);
     }
+    [Test]
+    public void CalculateAverage_SingleElementMatrix_ReturnsElementValue()
+    {
+	int[,] matrix = { { 5 } };
+
+    	double result = Server.CalculateAverage(matrix);
+
+        Assert.AreEqual(5, result);
+    }
+    [Test]
+    public void CalculateAverage_MatrixWithNegativeNumbers_ReturnsCorrectAverage()
+    {
+        int[,] matrix = {
+            {-1, -2, -3},
+            {-4, -5, -6},
+            {-7, -8, -9}
+    	};
+
+    	double result = Server.CalculateAverage(matrix);
+
+    	Assert.AreEqual(-5, result);
+    }
+    [Test]
+    public void CalculateAverage_MatrixWithZeros_ReturnsCorrectAverage()
+    {
+    	int[,] matrix = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        };
+
+        double result = Server.CalculateAverage(matrix);
+
+        Assert.AreEqual(0, result);
+    }
 }
